076c907ebba9392bad18ac191dedd6883cbdf15f
Dom231 <114959233+Dom231@users.noreply.github.com>
2022-11-22 12:33:03 -0600

    this will add the current day weather to the HTML

--- Assets/style.js
+++ Assets/style.js
@@ -2,11 +2,19 @@
 //Click event for the search button 
 var seachEl = document.getElementById("search-results");
 var cityInputEl = document.querySelector('#city');
-cityname = " ";
+var currentEl = document.getElementById("current-day");
+
+
+var cityname = " ";
+
+
 $(".sBtn").on("click",function(){
     cityname = $("#city").val();
     var cityLi = document.createElement('li')
     cityLi.textContent = cityname;
+    currentEl.textContent =" ";
+
+   
 
     console.log(cityname);
     seachEl.append(cityLi);
@@ -33,7 +41,42 @@ fetch(requestUrl)
     return response.json();
 })
 .then(function(data){
-    console.log(data.list);
+    console.log(data.list[0]);
+
+    wIcon = data.list[0].weather[0].icon;
+    console.log(wIcon);
+    var imgIcon = document.createElement('img')
+    imgIcon.setAttribute("src", "http://openweathermap.org/img/wn/"+wIcon+".png");
+    
+
+    var currentTittleEl = document.createElement('ul')
+    currentTittleEl.setAttribute("style","font-size: 50px;font-weight: bold")
+    currentTittleEl.textContent = cityname;
+    currentTittleEl.append(imgIcon);
+    currentEl.append(currentTittleEl);
+
+    var cityTemp = data.list[0].main.temp;
+    console.log(cityTemp);
+
+    var cityTempLi = document.createElement('ul')
+    cityTemp = 1.8*(cityTemp - 273.15)+32;
+    cityTempLi.textContent = Math.round(cityTemp)+'\u00b0'+"F";
+    currentEl.append(cityTempLi);
+
+ 
+    var cityHum = data.list[0].main.humidity;
+    console.log(cityHum);
+
+    var cityHumLi = document.createElement('ul')
+    cityHumLi.textContent = cityHum+"%";
+    currentEl.append(cityHumLi);
+
+    var cityWind = data.list[0].wind.speed;
+    console.log(cityWind)
+
+    var cityWindLi = document.createElement('ul')
+    cityWindLi.textContent = cityWind +"mph";
+    currentEl.append(cityWindLi);
 });
         
     });
